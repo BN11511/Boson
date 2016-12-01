12586de7a35f1d87f48b997cdc383e194df69b4b
7036tom <tom.dagens@hotmail.fr>
2016-12-01 19:10:57 +0100

    version2

--- Boson.py
+++ Boson.py
@@ -1,30 +1,47 @@
 # Create first network with Keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import SGD
+
 import numpy
-from keras.regularizers import l2, activity_l2
+import pandas
+from keras.regularizers import l1, activity_l1
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
+
 # load dataset
-dataset = numpy.loadtxt("training.csv", delimiter=",")
+dataframe = pandas.read_csv("training.csv", header=None)
+dataset = dataframe.values
 # split into input (X) and output (Y) variables
-X = dataset[:10000:,1:32]
-Y = dataset[:10000:,32]
+X = dataset[1:10000:,1:32].astype(float)
+Y = dataset[1:10000:,32].astype(float)
+X2 = dataset[10000:25000:,1:32].astype(float)
+Y2 = dataset[10000:25000:,32].astype(float)
 
 # create model
 model = Sequential()
-model.add(Dense(100, input_dim=31, init='uniform', activation='relu' ,W_regularizer=l2(0.000005), activity_regularizer=activity_l2(0.000005)))
-model.add(Dense(60, input_dim=31, init='uniform', activation='relu' ,W_regularizer=l2(0.00005), activity_regularizer=activity_l2(0.00005)))
-model.add(Dense(60, input_dim=31, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='softmax'))
+model.add(Dense(30, input_dim=31, init='normal', activation='relu' ,W_regularizer=l1(0.000001), activity_regularizer=activity_l1(0.000001)))
+model.add(Dense(30, init='normal', activation='relu' ))
+model.add(Dense(30, init='normal', activation='relu'))
+model.add(Dense(1, init='normal' , activation='sigmoid'))
 
 # Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#sgd = SGD(lr=1, momentum=0.5, decay=0.97, nesterov=True)
+model.compile(optimizer="RMSprop", loss='binary_crossentropy', metrics=['accuracy']) # Gradient descent
 # Fit the model
-model.fit(X, Y, nb_epoch=200, batch_size=96)
+
+model.fit(X2, Y2, nb_epoch=200, batch_size=96)
+
 # evaluate the model
 scores = model.evaluate(X, Y)
+
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 #test
+
+Z = model.predict(X, batch_size=32, verbose=0)
+print(Z[1:10])
+print(Y[1])
+
